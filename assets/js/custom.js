98c2203d1c6bfa5225ee08884f83782b63a5203c
ebr77 <134643518+ebr77@users.noreply.github.com>
2024-04-24 18:37:13 +0300

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,28 @@
+
+    // 1. En Basa Don
+		$(window).on('scroll',function () {
+			if ($(this).scrollTop() > 600) {
+				$('.return-to-top').fadeIn();
+			} else {
+				$('.return-to-top').fadeOut();
+			}
+		});
+		$('.return-to-top').on('click',function(){
+				$('html, body').animate({
+				scrollTop: 0
+			}, 1500);
+			return false;
+		});
+
+
+    // 2- Baslangic Animasyonu
+        $(window).load(function(){
+        	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".welcome-hero-serch-box").removeClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+        $(window).load(function(){
+        	$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".welcome-hero-serch-box").addClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
